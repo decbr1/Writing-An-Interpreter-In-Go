b91dd2990d0c1a5c884d6bb8583c831dc09be6e0
Dec <decbrks@pm.me>
2025-12-31 17:19:06 +0000

    1.5 - Start of a REPL

--- cmd/main.go
+++ cmd/main.go
@@ -5,3 +5,20 @@
 // ./cmd/main.go
 
 package main
+
+import (
+	"WritingAnInterpreterInGo/cmd/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! Welcome to the Monkey programming language.\n", user.Username)
+	fmt.Printf("This is the REPL. Feel free to type some commands.\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
