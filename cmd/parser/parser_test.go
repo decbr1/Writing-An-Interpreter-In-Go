1ee538f3efbcc5e6afcd55bb2ad91cb62be58e7e
Dec <decbrks@pm.me>
2026-01-15 13:07:39 +0000

    chapter 2 completed

--- cmd/parser/parser_test.go
+++ cmd/parser/parser_test.go
@@ -16,7 +16,7 @@ func TestLetStatements(t *testing.T) {
 		expectedValue      interface{}
 	}{
 		{"let x = 5;", "x", 5},
-		{"let let y = true;", "y", true},
+		{"let y = true;", "y", true},
 		{"let foobar = y;", "foobar", "y"},
 	}
 
--- cmd/repl/repl.go
+++ cmd/repl/repl.go
@@ -7,10 +7,23 @@ import (
 	"fmt"
 	"io"
 	"monkey/cmd/lexer"
-	"monkey/cmd/token"
+	"monkey/cmd/parser"
 )
 
 const PROMPT = ">> "
+
+const MONKEY_FACE = `            __,__ 
+.--.  .-"     "-.  .--. 
+/ .. \/  .-. .-.  \/ .. \ 
+| |  '|  /   Y   \  |'  | |
+| \   \  \ 0 | 0 /  /   / | 
+ \ '- ,\.-"""""""-./, -' / 
+  ''-' /_   ^ ^   _\ '-'' 
+      |  \._   _./  | 
+      \   \ '~' /  /
+      '._ '-=-' _.' 
+         '-----' 
+`
 
 // Start begins the REPL (Read Eval Print Loop). The REPL reads input, sends it to the
 // interpreter for evaluation, prints the result/output of the interpreter and starts again.
@@ -20,20 +33,31 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		// Print the prompt
 		fmt.Fprintf(out, PROMPT)
-		// Read from the input source until encountering a newline
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-		// Take the just-read line and pass it to an instance of our lexer
+
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		// Print all the tokens the lexer gives us until we encounter EOF
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "monkey error\n")
+	io.WriteString(out, " parser erros:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
