f963a02d40346d42a4345f1857ce1123481a276c
Dec <decbrks@pm.me>
2026-01-19 16:46:53 +0000

    feat: implement evaluator for AST nodes and integer expressions

---
+++ cmd/evaluator/evaluator.go
@@ -0,0 +1,33 @@
+// ./cmd/evaluator/evaluator.go
+
+package evaluator
+
+import (
+	"monkey/cmd/ast"
+	"monkey/cmd/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
