496299002ac5684a1018b9b951f20d6891f29099
Dec <decbrks@pm.me>
2026-02-15 18:09:48 +0000

    feat: add string support (evaluator updates)
    this part of a wider "commit" of `feat: add string support`. However, for my own sanity, ive split it up into various different commits.
    See the comment provided (in brackets) for more info.

--- cmd/evaluator/evaluator_test.go
+++ cmd/evaluator/evaluator_test.go
@@ -290,3 +290,16 @@ addTwo(2);`
 
 	testIntegerObject(t, testEval(input), 4)
 }
+
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World"`
+	expected := "Hello World"
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != expected {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
