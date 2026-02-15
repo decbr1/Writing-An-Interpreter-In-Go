091bd76985ad383e23b72b3ab5a9b876b2588b55
Dec <decbrks@pm.me>
2026-02-15 18:22:37 +0000

    feat: string concatentation
    example:
    ```
    decbr@Declans-MacBook-Air cmd % go run main.go
    Hello decbr! Welcome to the Monkey programming language.
    This is the REPL. Feel free to type some commands.
    >> let makeGreeter = fn(greeting) { fn(name) { greeting + " " + name + "!" } };
    >> let hello = makeGreeter("Hello");
    >> hello("Dia")
    Hello Dia!
    ```

--- cmd/evaluator/evaluator_test.go
+++ cmd/evaluator/evaluator_test.go
@@ -204,6 +204,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -301,5 +305,18 @@ func TestStringLiteral(t *testing.T) {
 	}
 	if str.Value != expected {
 		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	expected := "Hello World!"
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != expected {
+		t.Errorf("Sring has wrong value. got=%q", str.Value)
 	}
 }
